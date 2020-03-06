c4f17b45dcd16274b56671a3014090bbcfd7cedd
dolotkazin <tuxracer@yandex.ru>
2020-03-06 23:41:31 +0300

    Back up before huge refactoring

---
+++ FitCook/Controllers/KitchenWareCell.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class KitchenWareCell: UITableViewCell {
+
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var weightLabel: UILabel!
+	@IBOutlet weak var imageLabel: UIButton!
+	
+	override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+	@IBAction func imageButtonPressed(_ sender: UIButton) {
+		
+	}
+}
